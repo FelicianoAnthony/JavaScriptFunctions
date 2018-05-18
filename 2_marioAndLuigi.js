d74b5d49daccdce1008375cbcadf636e359888cd
FelicianoAnthony <afeliciano893@gmail.com>
2018-05-17 20:20:44 -0400

    codewars day 2 - (3)

---
+++ 2_marioAndLuigi.js
@@ -0,0 +1,26 @@
+/*
+The pipes connecting your level's stages together need to be fixed before you recieve any more complaints. 
+Each pipe should be connecting, since the levels ascend, 
+you can assume every number in the sequence after the first index will be greater than the previous and that there will be no duplicates.
+
+#Task Given the a list of numbers, return the list so that the values increment by 1 for each index up to the maximum value.
+
+#Example:
+Input: 1,3,5,6,7,8
+Output: 1,2,3,4,5,6,7,8
+
+*/
+
+
+
+
+
+function pipeFix(numbers){
+
+  let newArr = [];
+  for (let i=numbers[0]; i <= numbers[numbers.length-1]; i++) {
+    newArr.push(i)
+  }
+
+  return newArr;
+}
