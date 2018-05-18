b8bcd986fcee32362a5fdfedf5e471a99481ef39
FelicianoAnthony <afeliciano893@gmail.com>
2018-05-17 20:47:44 -0400

    codewars day 2 - (4)

---
+++ 2_codingMeetup.js
@@ -0,0 +1,37 @@
+/*
+...
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+your function should return the following array:
+
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+    greeting: 'Hi Madison, what do you like the most about Ruby?'
+  } 
+];
+
+
+*/
+
+function greetDevelopers(list) {
+
+  for (let i=0; i < list.length; i++) {
+    let obj = list[i];
+    let sentence = 'Hi ' + obj.firstName +  ', what do you like the most about ' + obj.language + '?'
+    obj['greeting'] = sentence
+  }
+
+  return list
+}
